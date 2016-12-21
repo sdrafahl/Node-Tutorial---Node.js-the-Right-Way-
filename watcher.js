79791dd5553a59504d24f85b84641687d824f1e2
Shane P Drafahl <sdrafahl@iastate.edu>
2016-12-21 12:13:19 -0600

    Added child process to monitor changes

--- watcher.js
+++ watcher.js
@@ -1,10 +1,13 @@
+"use strict";
 const 
-fs = require('fs');
+fs = require('fs'),
+spawn = require('child_process').spawn,
 filename = process.argv[2];
 if(!filename){
     throw Error("A file to watch must be specified");
 }
 fs.watch('target.txt', function (){
-    console.log("File 'target.txt' just changed! ");
+    let ls = spawn('ls' , ['-lh',filename]);
+    ls.stdout.pipe(process.stdout);
 });
 console.log("Now Watching " + filename + " for changes...");
