449c8db1002b38de6ab90d3c37974689fac8a711
Shane P Drafahl <sdrafahl@iastate.edu>
2016-12-21 12:35:02 -0600

    Added event listenters that listen to EventEmitter objects

--- watcher.js
+++ watcher.js
@@ -7,7 +7,17 @@ if(!filename){
     throw Error("A file to watch must be specified");
 }
 fs.watch('target.txt', function (){
-    let ls = spawn('ls' , ['-lh',filename]);
-    ls.stdout.pipe(process.stdout);
+   let 
+    ls = spawn('ls', ['-lh' , filename]),
+    output = '';
+   ls.stdout.on('data', function(chunk){
+       output += chunk.toString();
+   });
+
+   ls.on('close',function(){
+       let parts = output.split(/\s+/);
+       console.dir([parts[0],parts[4],parts[8]]);
+   });
+
 });
 console.log("Now Watching " + filename + " for changes...");
