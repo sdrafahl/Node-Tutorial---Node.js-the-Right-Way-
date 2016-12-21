77bdcc2599ac69bf1018d595ad5c2d477711addb
Shane P Drafahl <sdrafahl@iastate.edu>
2016-12-21 12:04:09 -0600

    Added exceptions and command line arguments

--- watcher.js
+++ watcher.js
@@ -1,5 +1,10 @@
-const fs = require('fs');
+const 
+fs = require('fs');
+filename = process.argv[2];
+if(!filename){
+    throw Error("A file to watch must be specified");
+}
 fs.watch('target.txt', function (){
     console.log("File 'target.txt' just changed! ");
 });
-console.log("Now Watching target.txt for changes...");
+console.log("Now Watching " + filename + " for changes...");
